0a9d824f7f99381974e99bdc2da4851d7d218abc
kmj4034 <kkmj4034@gmail.com>
2022-02-25 00:49:12 +0900

    220225

--- js/main.js
+++ js/main.js
@@ -18,15 +18,20 @@ searchInputEl.addEventListener('blur', function () {
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top'); 
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
   if (window.scrollY > 500) {
     // 배지 숨기기
     // gsap.to(요소, 지속시간, 옵션);
-    gsap.to(badgeEl, .6, {
+    gsap.to(badgeEl, .6, {   //gsap.to('#to-top', .6 {}) 도 가능하지만, to-top을 두 번 찾는 것은 비효율!
       opacity: 0,
       display: 'none'
+    });
+    // 버튼 보이기 !
+    gsap.to(toTopEl, .2, {   
+      x: 0
     });
     //badgeEl이 0.6초 동안 애니메이션 처리되면서,
     //opacity: 0으로 점점 수렴하게 된다.
@@ -36,9 +41,19 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 1,
       display: 'block'
     });
+    //버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100 
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in'); /*s붙이면 단수도 사용가능!*/
